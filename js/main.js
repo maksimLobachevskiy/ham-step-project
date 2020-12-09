9957939b1373264b7a6272a489a7ac3ce59d260e
Maksim Lobachevskiy <lobachevskiy.maksim@gmail.com>
2020-12-09 15:25:28 +0200

    1st section

---
+++ js/main.js
@@ -0,0 +1,15 @@
+"use strict";
+//Tabs
+let tab = document.querySelectorAll('.tabs-title');
+
+tab.forEach(function(item){
+    item.addEventListener('click', function(){
+        let currentTab = document.querySelector(`.tab-content[data-tab-content="${item.dataset.tab}"]`);
+
+        document.querySelector('.tab-content.content-active').classList.remove('content-active');
+        document.querySelector('.tabs-title.active').classList.remove('active');
+
+        currentTab.classList.add('content-active');
+        item.classList.add('active');
+    });
+});
